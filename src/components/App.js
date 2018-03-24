8d91fcaa079931ec89ee1700e640b98c4bb5a8a2
nknzh <18435186395@163.com>
2018-03-24 21:37:14 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>牛志昊</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'antd/dist/antd.css';
+import './styles/App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
