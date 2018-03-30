25780912893e5097379742a97608701be31daa57
bnm0781 <754467629@qq.com>
2018-03-30 20:47:41 +0800

    000

--- src/components/Jiangsu.js
+++ src/components/Jiangsu.js
@@ -1,124 +1,118 @@
 import React, { Component } from 'react';
 import { Table, Button } from 'antd';
-import reqwest from 'reqwest';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-const columns = [
-	{
-		title: '美食名称',
-		dataIndex: 'gourmetName',
-		// render: gourmetName => `${}`
-	},
-	{
-		title: '美食图片',
-		dataIndex: 'gourmetPic',
-		render: (gourmetPic) => {
-			return (
-				<img src={'http://10.9.163.109:3100/imgUploads/' + gourmetPic} width='100' height='100' alt="" />
-			);
-		}
-	},
-	{
-		title: '美食地区',
-		dataIndex: 'gourmetArea',
-		sorter: true,
-	},
-	{
-		title: '美食做法',
-		dataIndex: 'gourmetPrac'
-	},
-	{
-		title: '口味描述',
-		dataIndex: 'tasteDescri',
-		sorter: true,
-	},
-	{
-		title: '注意事项',
-		dataIndex: 'mattersAtt'
-	},
-	{
-		title: '上传时间',
-		dataIndex: 'uploadTime',
-		sorter: true,
-	},
-	{
-		title: '操作',
-		dataIndex: '',
-		render: () => {
-			return (
-				<div>
-					<Button type="primary" ghost className="positionChange">编辑</Button>
-					<Button type="danger" ghost className="positionChange">删除</Button>
-				</div>
-			);
+const mapStateToProps = (state) => {
+	return {
+		cuisineList: state
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getData: () => {
+			dispatch(() => {
+				fetch('/api/cuisine/area/32', {
+					method: 'get'
+				})
+				.then(response => response.json())
+				.then(result => {
+					dispatch({
+						type: 'GET_DATA',
+						dataList: result.data.map(({_id, gourmetName, gourmetPic, gourmetArea, gourmetPrac, tasteDescri, mattersAtt, uploadTime}) => ({
+							key: _id,
+							gourmetName,
+							gourmetPic,
+							gourmetArea,
+							gourmetPrac,
+							tasteDescri,
+							mattersAtt,
+							uploadTime
+						}))
+					})
+				})
+			})
 		}
 	}
-];
-
+}
 
 class Jiangsu extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			data: [], // 数据
-			pagination: {}, // 页码
-			loading: false, // 是否加载
+				columns: [
+				{
+					title: '美食名称',
+					dataIndex: 'gourmetName',
+				},
+				{
+					title: '美食图片',
+					dataIndex: 'gourmetPic',
+					render: (gourmetPic) => {
+						return (
+							<img src={'http://10.9.163.109:3100/imgUploads/' + gourmetPic} width='100' height='100' alt="" />
+						);
+					}
+				},
+				{
+					title: '美食地区',
+					dataIndex: 'gourmetArea',
+					sorter: true,
+				},
+				{
+					title: '美食做法',
+					dataIndex: 'gourmetPrac'
+				},
+				{
+					title: '口味描述',
+					dataIndex: 'tasteDescri',
+					sorter: true,
+				},
+				{
+					title: '注意事项',
+					dataIndex: 'mattersAtt'
+				},
+				{
+					title: '上传时间',
+					dataIndex: 'uploadTime',
+					sorter: true,
+				},
+				{
+					title: '操作',
+					dataIndex: '',
+					render: (text) => {
+						return (
+							<div>
+								<NavLink to={'/home/edit/' + text.key} className="ant-btn positionChange ant-btn-primary ant-btn-background-ghost">编辑</NavLink>
+								<Button type="danger" ghost className="positionChange" onClick={() => this.deleteData()}>删除</Button>
+							</div>
+						);
+					}
+				}
+			]
 		};
 	}
 
-	/* 页码记录 */
-	handleTableChange = (pagination, fileters, sorter) => {
-		const pager = { ...this.state.pagination };
-		pager.current = pagination.current; // 当前页码
-		this.setState({
-			pagination: pager,
-		});
-		this.fetch({
-			results: pagination.pageSize,
-			page: pagination.current,
-			sortField: sorter.field,
-			sortOrder: sorter.order,
-		});
-	}
-
-	/* 后台数据请求 */
-	fetch = (params = {}) => {
-		this.setState({ loading: true });
-		reqwest({
-			url: '/api/cuisine/area/51',
-			type: 'json',
-			method: 'get',
-			contentType: 'application/json',
-			data: {
-				results: 10,
-				...params,
-			}
-		})
-		.then((data) => {
-			const pagination = { ...this.state.pagination };
-			pagination.total = 200; // 数据总数
-			this.setState({
-				loading: false,
-				data: data.data,
-				pagination,
-			});
-		});
-	}
-
 	componentDidMount() {
-		this.fetch();
+		this.props.getData();
+	}
+
+	deleteData() {
+		console.log(this.props.cuisineList)
 	}
 
 	render() {
 		return (
-			<Table columns = { columns }
-				rowKey = { record => record.registered }
-				dataSource = { this.state.data }
-				pagination = { this.state.pagination }
-				loading = { this.state.loading }
-				onChange = { this.handleTableChange }
-			/>
+			<Table columns={this.state.columns} dataSource={this.props.cuisineList} pagination={{
+				'defaultCurrent': 1,
+				'defaultPageSize': 5,
+				'showSizeChanger': true,
+				'pageSizeOptions': ['5', '10', '15'],
+				'showQuickJumper': true
+			}}/>
 		);
-	}
+	};
 }
 
-export default Jiangsu;
+export default connect(mapStateToProps, mapDispatchToProps)(Jiangsu);
